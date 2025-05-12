4c81b5b014be0ff13c3a918870c6e87ba3b9d2cb
user1 <user01@em/com>
2025-05-12 05:22:55 +0300

    c1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let welcome_msg = document.getElementById("welcome");
+let username1 = localStorage.getItem("username");
+
+if (username1 != null){
+    welcome_msg.innerText = `welcome back ${username1}`;
+}
+
+
+
